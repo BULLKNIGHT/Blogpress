aa6b27a402adc0bf3ce241321b5a24531d643ae3
Molay Halder <malayhalder70@gmail.com>
2021-01-08 03:50:44 +0530

    v 2.0

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,27 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const session = require("express-session");
+
+const session_secret = "Blogpress";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  credentials: true,
-  origin: "*"
-}))
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
+
+app.use(
+  session({
+    secret: session_secret,
+    cookie: { maxAge: 1 * 60 * 60 * 1000 },
+  })
+);
 
 // DB Connection
 require("./db");
@@ -16,9 +29,10 @@ require("./db");
 // Models
 
 // Routes calls
-app.use('/', require('./routes/user'));
+app.use("/", require("./routes/user"));
+app.use("/blogs", require("./routes/blog"));
+app.use("/rates", require("./routes/rate"));
 
-
-app.listen(3000, () => {
-  console.log("Server listen at port 3000!");
+app.listen(9999, () => {
+  console.log("Server listen at port 9999!");
 });
