16478354261e916075123249a56689ce3ad3319a
Molay Halder <malayhalder70@gmail.com>
2021-01-09 04:56:52 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,19 @@
 import React, { useState } from "react";
 import "../styles/App.css";
 import Home from "./Home";
-import Form from "./Form"
+import Form from "./Form";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
   const [user, setUser] = useState({});
+
   const userLogin = (userObj) => {
-    setUser({userObj});
+    setUser({ userObj });
     setIsLogin(true);
-  }
+  };
   return (
-    <>
-      {isLogin? <Home user={user}/> : <Form loginHandler={userLogin}/>}
-    </>
+    <>{isLogin ? <Home user={user} /> : <Form loginHandler={userLogin} />}</>
   );
 }
 
 export default App;
-
